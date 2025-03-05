d95235fe82b964e3c7296a7c6119db7075da331b
KB <kaabutu@gmail.com>
2025-03-05 12:44:56 -0800

    skeleton

---
+++ app/main.py
@@ -0,0 +1,23 @@
+from aws_lambda_powertools import Logger, Tracer
+from aws_lambda_powertools.utilities.typing import LambdaContext
+from aws_lambda_powertools.event_handler import APIGatewayRestResolver
+
+# Initializations outside handler to keep warm
+tracer = Tracer()
+logger = Logger()
+app = APIGatewayRestResolver()
+
+@app.post("/shorten")
+def create_url():
+    ...
+
+@app.get("/urls")
+def fetch_all_urls():
+    ...
+
+@app.delete("/delete-url")
+def create_url():
+    ...
+
+def lambda_handler(event: dict, context: LambdaContext) -> dict:
+    return app.resolve(event, context)
